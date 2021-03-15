a4309e2da7ac1a91ebdaca17a389cf61e1594bcb
Silvia <48796281+sigeek@users.noreply.github.com>
2021-03-15 15:19:18 +0100

    Added simulation

--- simulations/SIRtrials.m
+++ simulations/SIRtrials.m
@@ -10,6 +10,7 @@ N = 60.*10^6;
 [data, dates] = getData;
 I = cast((data.totale_positivi), 'double'); 
 R = cast((data.dimessi_guariti), 'double');
+
 X0= [N I(1) R(1)]; 
 %% simulation
 
@@ -25,16 +26,3 @@ plot(t,S,'b',t,I,'r',t,R,'g','LineWidth',2);
 xlabel('Days'); ylabel('Number of individuals');
 legend('S','I','R'); title('SIR model')
 
-%% simulation
-
-beta = 0.008;      
-gamma = 0.01; 
-p = [beta, gamma];
-timeV = 1:1:50;
-err = rand(1, 50);
-[t,X] = ode23s(@(t,x) SIR(t,x,p), timeV, X0);  
-S = X(:, 1); I = X(:, 2); R = X(:, 3);
-
-plot(t,S,'b',t,I,'r',t,R,'g','LineWidth',2); 
-xlabel('Days'); ylabel('Number of individuals');
-legend('S','I','R'); title('SIR model')
