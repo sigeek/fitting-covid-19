4def462f609826877a62c634de1e38adb5d2a3df
Silvia <48796281+sigeek@users.noreply.github.com>
2021-03-20 11:43:36 +0100

    Update SEIIRHD simulation

--- experiments/simulations/ITA.m
+++ experiments/simulations/ITA.m
@@ -24,92 +24,113 @@ H = cast(data.totale_ospedalizzati, 'double');
 D = cast(data.deceduti, 'double');
 
 t0 = 1;
-E0 = E(t0);
-I0 = I(t0);
-H0 = H(t0);
-R0 = R(t0);
-D0 = D(t0);
+E0 = 0;
+I0 = 1;
+H0 = 0;
+R0 = 0;
+D0 = 0;
 S0 = N-E0-I0-R0;
 
 timeV = 0:1:50;
-%% R0 = 0.5
-f1 = 0.5;
+%%
+
+lb = [0.4, 0.15, 0, 0, 0, 0, 0];
+ub = [0.8, 0.35, 0.1, 0.5, 0.5, 0.1, 0.1];
+
+
+f = unifrnd(lb(1),ub(1),1,1000);
+alpha = unifrnd(lb(2),ub(2),1,1000);
+gamma = unifrnd(lb(3),ub(3),1,1000);
+beta_a = unifrnd(lb(4),ub(4),1,1000);
+beta_s = unifrnd(lb(5),ub(5),1,1000);
+nu_s = unifrnd(lb(6),ub(6),1,1000);
+mu = unifrnd(lb(7),ub(7),1,1000);
+
+% compute quartiles
+fq = quantile(f,[0.25, 0.5, 0.75]);
+alphaq = quantile(alpha,[0.25, 0.5, 0.75]);
+gammaq = quantile(gamma,[0.25, 0.5, 0.75]);
+beta_aq = quantile(beta_a,[0.25, 0.5, 0.75]);
+beta_sq = quantile(beta_s,[0.25, 0.5, 0.75]);
+nu_sq = quantile(nu_s,[0.25, 0.5, 0.75]);
+muq = quantile(mu,[0.25, 0.5, 0.75]);
+%% quartile 25%
+i = 1;
+
+f1=fq(i);
 I_a1 = (1-f1)*I;
 I_s1 = f1*I;
 I_a10 = I_a1(t0);
 I_s10 = I_s1(t0);
 X01= [S0 E0 I_a10 I_s10 H0 R0 D0];
 
-alpha1 = 0.3;
-gamma1 = 0.041;
-beta_a1 = 0.021;
-beta_s1 = 0.023;
-nu_s1 = 0.004;
-mu1 = 0.002;
+alpha1 = alphaq(i);
+gamma1 = gammaq(i);
+beta_a1 = beta_aq(i);
+beta_s1 = beta_sq(i);
+nu_s1 = nu_sq(i);
+mu1 = muq(i);
 
 
 R01 = (f1*beta_s1)/(gamma1+mu1+nu_s1) + (1-f1)*beta_a1/gamma1;
-fprintf("R0 first simulation: %f \n", R01); 
+fprintf("R0 quartile 0.25: %f \n", R01); 
 p1 = [f1, alpha1, gamma1, beta_a1, beta_s1, nu_s1, mu1];
 [t_res,x_res1] = ode23s(@(t,x) SEIIRHD(t,x, p1), timeV, X01);  
-%% R0 = 1
-f2 = 0.55;
+%% quartile 50%
+i = 2;
+
+f2=fq(i);
 I_a2 = (1-f2)*I;
 I_s2 = f2*I;
 I_a20 = I_a2(t0);
 I_s20 = I_s2(t0);
 X02= [S0 E0 I_a20 I_s20 H0 R0 D0];
 
-alpha2 = 0.3;
-gamma2 = 0.0425;
-beta_a2 = 0.05;
-beta_s2 = 0.052;
-nu_s2 = 0.01;
-mu2 = 0.008;
+alpha2 = alphaq(i);
+gamma2 = gammaq(i);
+beta_a2 = beta_aq(i);
+beta_s2 = beta_sq(i);
+nu_s2 = nu_sq(i);
+mu2 = muq(i);
 
 R02 = (f2*beta_s2)/(gamma2+mu2+nu_s2) + (1-f2)*beta_a2/gamma2;
-fprintf("R0 second simulation: %f \n", R02); 
+fprintf("R0 quartile 0.5: %f \n", R02); 
 p2 = [f2, alpha2, gamma2, beta_a2, beta_s2, nu_s2, mu2];
 [t_res,x_res2] = ode23s(@(t,x) SEIIRHD(t,x, p2), timeV, X02);  
-%% R0 = 1.5
-f3 = 0.7;
+%% quartile 75%
+i=3;
+
+f3=fq(i);
 I_a3 = (1-f3)*I;
 I_s3 = f3*I;
 I_a30 = I_a3(t0);
 I_s30 = I_s3(t0);
 X03 = [S0 E0 I_a30 I_s30 H0 R0 D0];
 
-alpha3 = 0.3;
-gamma3 = 0.035;
-beta_a3 = 0.081;
-beta_s3 = 0.081;
-nu_s3 = 0.0245;
-mu3 = 0.0105;
+alpha3 = alphaq(i);
+gamma3 = gammaq(i);
+beta_a3 = beta_aq(i);
+beta_s3 = beta_sq(i);
+nu_s3 = nu_sq(i);
+mu3 = muq(i);
+
 
 R03 = (f3*beta_s3)/(gamma3+mu3+nu_s3) + (1-f3)*beta_a3/gamma3;
-fprintf("R0 third simulation: %f \n", R03); 
+fprintf("R0 quartile 0.75: %f \n", R03); 
 p3 = [f3, alpha3, gamma3, beta_a3, beta_s3, nu_s3, mu3];
 [t_res,x_res3] = ode23s(@(t,x) SEIIRHD(t,x, p3), timeV, X03); 
 
-%% R0 = 3
-f4 = 0.8;
-I_a4 = (1-f4)*I;
-I_s4 = f4*I;
-I_a40 = I_a4(t0);
-I_s40 = I_s4(t0);
-X04 = [S0 E0 I_a40 I_s40 H0 R0 D0];
 
-alpha4 = 0.3;
-gamma4 = 0.0375;
-beta_a4 = 0.24;
-beta_s4 = 0.21;
-nu_s4 = 0.03;
-mu4 = 0.03;
-
-R04 = (f4*beta_s4)/(gamma4+mu4+nu_s4) + (1-f4)*beta_a4/gamma4;
-fprintf("R0 forth simulation: %f \n", R04); 
-p4 = [f4, alpha4, gamma4, beta_a4, beta_s4, nu_s4, mu4];
-[t_res,x_res4] = ode23s(@(t,x) SEIIRHD(t,x, p4), timeV, X04); 
-%%
-simulate_SEIIRHD(timeV, x_res1, x_res2, x_res3, x_res4,...
+%% plot
+simulate_SEIIRHD(timeV, x_res1, x_res2, x_res3,...
     "./results/simulations/ITA/SEIIRHD.png")
+
+%% sensitivity analysis
+den = gamma1 + mu1 + nu_s1;
+
+c_f = (beta_s1/den - beta_a1/gamma1)*(f1/R01);
+c_betaa = ((1-f1)/gamma1)*beta_a1/R01;
+c_betas = (f1/den)*beta_s1/R01;
+c_gamma = (-((f1*beta_s1)/den)^2-(((1-f1)*beta_a1)/gamma1)^2)*gamma1/R01;
+c_nus = (-((f1*beta_s1)/den)^2)*nu_s1/R01;
+c_mu = (-((f1*beta_s1)/den)^2)*mu1/R01;
--- models/SEIIRHD/simulate_SEIIRHD.m
+++ models/SEIIRHD/simulate_SEIIRHD.m
@@ -1,11 +1,10 @@
-function simulate_SEIIRHD(timeV, X1, X2, X3, X4, path)
+function simulate_SEIIRHD(timeV, X1, X2, X3, path)
 
 % Input 
 % timeV   time vector used as x-axis
-% X1      vector containg the ODE solution with R0 = 0.5
-% X2      vector containg the ODE solution with R0 = 1
-% X3      vector containg the ODE solution with R0 = 1.5
-% X4      vector containg the ODE solution with R0 = 3
+% X1      vector containg the ODE solution with 0.25 quartiles
+% X2      vector containg the ODE solution with 0.5 quartiles
+% X3      vector containg the ODE solution with 0.75 quartiles
 % path    path for saving the plot obtained
 
 % Output
@@ -26,31 +25,22 @@ I_s3 = X3(:, 4);
 H3 = X3(:, 5);
 D3 = X3(:, 7);
 
-I_a4 = X4(:, 3);
-I_s4 = X4(:, 4);
-H4 = X4(:, 5);
-D4 = X4(:, 7);
 
-fprintf("R0 = 0.5: "); 
+fprintf("0.25 Quartiles: "); 
 fprintf("Max I_a: %f, ", max(I_a1)); 
 fprintf("Max I_s: %f\n", max(I_s1)); 
 fprintf("Max H: %f, ", max(H1)); 
 fprintf("Max D: %f\n", max(D1)); 
-fprintf("R0 = 1: "); 
+fprintf("0.5 Quartiles: "); 
 fprintf("Max I_a: %f, ", max(I_a2)); 
 fprintf("Max I_s: %f\n", max(I_s2)); 
 fprintf("Max H: %f, ", max(H2)); 
 fprintf("Max D: %f\n", max(D2)); 
-fprintf("R0 = 1.5: "); 
+fprintf("0.75 Quartiles: "); 
 fprintf("Max I_a: %f, ", max(I_a3)); 
 fprintf("Max I_s: %f\n", max(I_s3)); 
 fprintf("Max H: %f, ", max(H3)); 
 fprintf("Max D: %f\n", max(D3)); 
-fprintf("R0 = 3"); 
-fprintf("Max I_a: %f, ", max(I_a4)); 
-fprintf("Max I_s: %f\n", max(I_s4)); 
-fprintf("Max H: %f, ", max(H4)); 
-fprintf("Max D: %f\n", max(D4)); 
 
 x0=100;
 y0=100;
@@ -63,10 +53,9 @@ subplot(2,2,1);
 plot(timeV,I_a1,'g-', 'Linewidth', 1.5);hold on
 plot(timeV,I_a2,'b-', 'Linewidth', 1.5);hold on
 plot(timeV,I_a3,'r-', 'Linewidth', 1.5);hold on
-plot(timeV,I_a4,'k-', 'Linewidth', 1.5);hold on
 xlabel('Days');ylabel('Number of individuals');
-legend('R0 = 0.5', 'R0 = 1' ,...
-    'R0 = 1.5', 'R0 = 3' ,...
+legend('R0 = 3.2247', 'R0 = 2.9287' ,...
+    'R0 = 2.5396', ...
     'Location', 'northeast');
 %set(gca,'XLim',[timeV(1), dates(tf+tp)]);
 title(sprintf('Infected Asymptomatic'));
@@ -76,10 +65,9 @@ subplot(2,2,2);
 plot(timeV,I_s1,'g-', 'Linewidth', 1.5);hold on
 plot(timeV,I_s2,'b-', 'Linewidth', 1.5);hold on
 plot(timeV,I_s3,'r-', 'Linewidth', 1.5);hold on
-plot(timeV,I_s4,'k-', 'Linewidth', 1.5);hold on
 xlabel('Days');ylabel('Number of individuals');
-legend('R0 = 0.5', 'R0 = 1' ,...
-    'R0 = 1.5', 'R0 = 3' ,...
+legend('R0 = 3.2247', 'R0 = 2.9287' ,...
+    'R0 = 2.5396', ...
     'Location', 'northeast');
 %set(gca,'XLim',[timeV(1), dates(tf+tp)]);
 title(sprintf('Infected Symptomatic'));
@@ -89,10 +77,9 @@ subplot(2,2,3);
 plot(timeV,H1,'g-', 'Linewidth', 1.5);hold on
 plot(timeV,H2,'b-', 'Linewidth', 1.5);hold on
 plot(timeV,H3,'r-', 'Linewidth', 1.5);hold on
-plot(timeV,H4,'k-', 'Linewidth', 1.5);hold on
 xlabel('Days');ylabel('Number of individuals');
-legend('R0 = 0.5', 'R0 = 1' ,...
-    'R0 = 1.5', 'R0 = 3' ,...
+legend('R0 = 3.2247', 'R0 = 2.9287' ,...
+    'R0 = 2.5396',...
     'Location', 'northeast');
 %set(gca,'XLim',[timeV(1), dates(tf+tp)]);
 title(sprintf('Hospitalized'));
@@ -102,14 +89,12 @@ subplot(2,2,4);
 plot(timeV,D1,'g-', 'Linewidth', 1.5);hold on
 plot(timeV,D2,'b-', 'Linewidth', 1.5);hold on
 plot(timeV,D3,'r-', 'Linewidth', 1.5);hold on
-plot(timeV,D4,'k-', 'Linewidth', 1.5);hold on
 xlabel('Days');ylabel('Number of individuals');
-legend('R0 = 0.5', 'R0 = 1' ,...
-    'R0 = 1.5', 'R0 = 3' ,...
+legend('R0 = 0.8282', 'R0 = 0.6986' ,...
+    'R0 = 0.5175',...
     'Location', 'northeast');
 %set(gca,'XLim',[timeV(1), dates(tf+tp)]);
 title(sprintf('Deaths'));
-
 saveas(gcf,path)
 
 
