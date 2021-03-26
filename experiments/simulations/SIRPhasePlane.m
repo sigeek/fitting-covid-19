73a335903a6bd6f1700ea81bc947581121d2146f
Silvia <48796281+sigeek@users.noreply.github.com>
2021-03-26 20:19:01 +0100

    Updated plots

--- experiments/simulations/SIRPhasePlane.m
+++ experiments/simulations/SIRPhasePlane.m
@@ -6,8 +6,8 @@ mu=5e-5;
 
 SIR=@(t,y)[mu-beta*y(1)*y(2)-mu*y(1);beta*y(1)*y(2)-gamma*y(2)-mu*y(2)];
 
-y1=linspace(0,1,20);
-y2=linspace(0,1,20);
+y1=linspace(0,1,14);
+y2=linspace(0,1,14);
 [x,y]=meshgrid(y1,y2);
 u=zeros(size(x));
 v=zeros(size(y));
@@ -27,11 +27,12 @@ title('Phase Plane SIR');
 hold on
 
 %solve ODEs
-for y10=[0:0.2:1]
-    for y20=[0:0.2:1]
+for y10=[0:1:1]
+    for y20=[0:0.05:0.7]
     options=odeset('MaxStep',0.1);
     [ts,ys]=ode45(SIR,[0,4000],[y10,y20],options);
-    plot(ys(:,1),ys(:,2))
+    plot(ys(:,1),ys(:,2), 'Linewidth', 1.5)
+    hold on
     end
 end
 saveas(gcf,"./results/simulations/SIRPhasePlane.png")
